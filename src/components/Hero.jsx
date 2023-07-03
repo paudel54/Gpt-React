195bc229c5c6e3917469dc86581eff45599c3e3f
paudel54 <2sansrit@gmail.com>
2023-07-03 18:08:16 +0545

    into production mode

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -15,8 +15,11 @@ const Hero = () => {
       </nav>
       <h1 className="head_text">
         {/* we can control tailwnd default property of minWidth- explesitely with max width;; */}
-        Summarize Articles with <br className="max-md:hidden" />
-        <span className="orange_gradient">OpenAI GPT-4</span>
+        Summarize Articles with <br /> Paieu
+        <br className="max-md:hidden" />
+        <span className="orange_gradient">
+          AI for your's <br /> Document Summary.{" "}
+        </span>
       </h1>
       <h2 className="desc">
         Simplifiy your reading with Summize, an open-Source article summarizer
